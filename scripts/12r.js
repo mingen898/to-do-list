b7bd7159aa936ad9f70dfe2cb5a9d884c6a95480
mingen898 <153106557+mingen898@users.noreply.github.com>
2026-01-08 14:47:07 +0900

    Add files via upload

---
+++ scripts/12r.js
@@ -0,0 +1,191 @@
+let score = JSON.parse(localStorage.getItem('score')) ||  {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+
+/*  
+if (!score) {
+score = {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+}
+}
+*/
+updateScoreElement();
+
+function updateScoreElement() {
+document.querySelector('.js-score')
+  .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+
+}
+
+resetScore = () => {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem('score');
+  updateScoreElement();
+};
+
+function pickComputerMove() {
+const randomNumber = Math.random();
+
+let computerMove = '';
+
+if (randomNumber >= 0 && randomNumber < 1/3) {
+  computerMove = 'Rock';
+} else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+  computerMove = 'Paper';
+} else if (randomNumber >= 2/3 && randomNumber < 1){
+  computerMove = 'Scissors';
+}
+
+return computerMove;
+}
+
+document.querySelector('.js-rock-button')
+  .addEventListener('click', () => {
+    playGame('Rock');
+  });
+
+document.querySelector('.js-paper-button')
+  .addEventListener('click', () => {
+    playGame('Paper');
+  });
+
+document.querySelector('.js-scissors-button')
+  .addEventListener('click', () => {
+    playGame('Scissors');
+  });
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('Rock');
+  } else if (event.key === 'p') {
+    playGame('Paper');
+  } else if (event.key === 's') {
+    playGame('Scissors');
+  } else if (event.key === 'a') {
+    autoPlay();
+  } else if (event.key === 'Backspace') {
+    resetScoreConfirmation();
+  }
+});
+
+function playGame(playerMove) {
+const computerMove = pickComputerMove();
+
+let result = '';
+
+if (playerMove === 'Scissors') {
+  if (computerMove === 'Rock') {
+    result = 'You lose.'
+  } else if (computerMove === 'Paper') {
+    result = 'You win!.'
+  } else if (computerMove === 'Scissors') {
+    result = 'Tie.'
+  }
+
+} else if (playerMove === 'Paper') {
+  if (computerMove === 'Rock') {
+    result = 'You win!'
+  } else if (computerMove === 'Paper') {
+    result = 'Tie.'
+  } else if (computerMove === 'Scissors') {
+    result = 'You lose.'
+  }
+  
+} else if (playerMove === 'Rock') {
+  if (computerMove === 'Rock') {
+    result = 'Tie.'
+  } else if (computerMove === 'Paper') {
+    result = 'You lose.'
+  } else if (computerMove === 'Scissors') {
+    result = 'You win!'
+  }
+}
+
+if (result === 'You win!') {
+  score.wins += 1;
+} else if (result === 'You lose.') {
+  score.losses += 1;
+} else if (result === 'Tie.') {
+  score.ties += 1;
+}
+
+localStorage.setItem('score', JSON.stringify(score));
+
+updateScoreElement();
+
+document.querySelector('.js-result').innerHTML = result;
+
+document.querySelector('.js-moves').innerHTML = `You
+<img class="move-icon" src="images/${playerMove}-emoji.png">
+<img class="move-icon" src="images/${computerMove}-emoji.png">
+Computer`
+
+}
+
+let isAutoPlay = false;
+
+let intervalId;
+
+function autoPlay() {
+  if (!isAutoPlay) {
+    intervalId = setInterval(() => {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    document.querySelector('.auto-play-button')
+    .innerHTML = 'Stop Playing';
+    isAutoPlay = true;
+  } else {
+    clearInterval(intervalId);
+    document.querySelector('.auto-play-button')
+    .innerHTML = 'Auto Play';
+    isAutoPlay = false;
+  }
+}
+
+let autoPlayButton = document.querySelector('.js-auto-play-button');
+autoPlayButton.addEventListener('click', () => {
+  autoPlay();
+});
+
+let confirmationText = document.querySelector('.js-confirmation-text');
+
+let resetScoreConfirmation = () => {
+  confirmationText.innerHTML = `
+  <div style="
+    display: flex;
+    flex-direction: row; 
+    align-items: center; 
+  ">
+    <div style="
+      margin-right: 10px;
+    ">Are you sure you want to reset the score?</div>
+    <button class="yes-button js-yes-button">Yes</button>
+    <button class="no-button js-no-button">No</button>
+  </div>
+  `
+  let yesButton = document.querySelector('.js-yes-button');
+  let noButton = document.querySelector('.js-no-button');
+
+  yesButton.addEventListener('click', () => {
+    confirmationText.innerHTML = '';
+    resetScore();
+  })
+
+  noButton.addEventListener('click', () => {
+    confirmationText.innerHTML = '';
+  })
+};
+
+let resetScoreButton = document.querySelector('.js-reset-score-button');
+
+resetScoreButton.addEventListener('click', () => {
+  resetScoreConfirmation();
+});
+
